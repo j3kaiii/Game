7c434824eaaa65543a301edbcdb2d2010c57de69
j3kaiii <j3kaiii@gmail.com>
2019-07-20 21:23:49 +0300

    класс Sprite

--- core/src/ru/tretyakov/screen/MenuScreen.java
+++ core/src/ru/tretyakov/screen/MenuScreen.java
@@ -3,44 +3,32 @@ package ru.tretyakov.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.tretyakov.base.BaseScreen;
+import ru.tretyakov.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
-    public static final float V_SPEED = 0.01f;
-
-    private Vector2 myPos;
-    private Vector2 end;
-    private Vector2 buf;
-    private Vector2 v;
-
     private Texture img;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        v = new Vector2();
-        myPos = new Vector2();
-        end = new Vector2();
-        buf = new Vector2();
+        logo = new Logo(new TextureRegion(img));
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        buf.set(end);
-        if (buf.sub(myPos).len() > V_SPEED) {
-            myPos.add(v);
-        } else {
-            myPos.set(end);
-        }
+        logo.update(delta);
         Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, myPos.x, myPos.y, 0.15f, 0.15f);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -52,8 +40,7 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        end.set(touch);
-        v.set(touch.sub(myPos)).setLength(V_SPEED);
+        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
